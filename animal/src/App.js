4201af20921542daf8560ae1c48868d4f4b2297d
guddnboy <hung3018@naver.com>
2025-01-14 16:37:42 +0900

    feat: SPA로 컴포넌트 호출

--- animal/src/App.js
+++ animal/src/App.js
@@ -1,3 +1,33 @@
-export default function App({$app}){
+import Content from './components/content';
+import TabBar from './components/tabBar';
+import {request} from './components/api';
 
+export default function App({$app}){
+    this.state = {
+        currentTab : 'all',
+        photos : []
+    }
+
+    const tabBar = new TabBar({$app, initialState:''});
+    const content = new Content({$app, initialState:[]});
+
+    this.setState =(newState) =>{
+        this.state = newState;
+        tabBar.setState(this.state.currentTab);
+        content.setState(this.state.photos);
+    }
+
+    const init = async () => {
+        try {
+            const initialPhotos = await request();
+            this.setState({
+                ...this.state,
+                photos: initialPhotos
+            });
+        } catch (error) {
+            console.log(error);   
+        }
+    }
+
+    init();
 }
